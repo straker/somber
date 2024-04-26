8c6263241b74423fa0956d15c9a3bc4328e7b6fc
straker <2433219+straker@users.noreply.github.com>
2024-04-26 17:33:08 -0600

    ignore prettier on long line

--- src/parse.js
+++ src/parse.js
@@ -3,14 +3,15 @@
 //
 // 1. Valid variable name start character
 // 2. Valid variable name characters after the start
-// 3. Match a dot, array access, or optional chain (e.g. allow "state.b", "state['b']", and "state?.thing") to match as single path
+// 3. Match a dot, array access, or optional chain (e.g. allow "state.b", "state['b']", and "state?.thing" to match as a single path)
 //
 //                             1                             2                                    3
 //                     ┏━━━━━━━┻━━━━━━━━┓┏━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━┓ ┏━━━━━━━━━━━┻━━━━━━━━━━┓
+// prettier-ignore
 const pathRegexStr =
   '([\\p{L}\\p{Nl}$_#][\\p{L}\\p{Nl}$\\p{Mn}\\p{Mc}\\p{Nd}\\p{Pc}]*(\\?*\\.|\\[[^\\]]+\\])*)+';
 
-// throw out any match that ends with a "(" (e.g. a function call)
+// throw out any match that ends with a "(" (i.e. a function call)
 // @see https://stackoverflow.com/a/48140275/2124254
 const pathRegex = new RegExp(
   `(?!${pathRegexStr}\\s*\\()${pathRegexStr}`,
